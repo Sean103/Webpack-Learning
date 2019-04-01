595b14a58cc660df247bc52ebfce3ead50ddc388
xiaoshengkang <898987943@qq.com>
2019-04-01 20:32:13 +0800

    使用webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 var path = require('path');
+var webpack=require("webpack");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './app/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devtool: 'inline-source-map',
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+
 };
